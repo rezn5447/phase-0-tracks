52db94b327998490b9efeb5b1daae8e413dad670
rezn5447 <rezn5447@gmail.com>
2016-08-03 13:51:34 -0700

    Add first series of questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+name = ""
+age = 0
+born = 0
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+born = gets.chomp
+
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+bread = gets.chomp 
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp 
