c7a73c67a276e3be3449557d9e81809f213a89e8
rezn5447 <rezn5447@gmail.com>
2016-08-03 22:08:47 -0700

    Almost done with 4-4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,19 +1,64 @@
-name = ""
-age = 0
-born = 0
+def survey 
 
 puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
+age = age.to_i
 
 puts "What year were you born?"
-born = gets.chomp
+year = gets.chomp
+year = year.to_i
+if year == 0 
+puts "you didn't really put a year!"
+end
 
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+  	bread = gets.chomp
+    until bread == "yes" || bread == "no"
+	puts "I'm sorry, please only use 'yes' or 'no' for this question"
+     bread = gets.chomp 
+     end
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-bread = gets.chomp 
+puts "Would you like to enroll in the company's health insurance? (yes or no)"
+	insurance = gets.chomp
+	until insurance == "yes" || insurance == "no"
+	puts	"I'm sorry, please only use 'yes' or 'no' for this question"
+	insurance = gets.chomp 
+	end
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp 
+	 if age < 100 && (2016 - year = age || 2015 - year = age)
+
+		if bread == "yes" && (name != "Drake Cula" || name != "Tu Fang")
+			
+		result = "Probably not a vampire";
+	
+		end
+	
+		
+		
+	elsif age > 100 || (2016 - year != age || 2015 - year != age )
+
+	 	if bread == "no" || insurance == "no"
+		result = "Probably a vampire"
+
+		end
+	
+	elsif age < 100 && (2016 - year = age || 2015 - year = age)
+	
+		if bread == "yes" && insurance == "yes"
+			if name == "Drake Cula" || name == "Tu Fang"
+	
+		result = "Clearly a vampire"
+		end
+	end
+
+    else
+    	result = "Results are inconclusive"
+    end
+    puts "#{result}"
+    
+end
+
+survey()
