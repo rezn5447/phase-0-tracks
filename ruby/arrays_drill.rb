02984213f158e9766d27e884619da33353cd27e2
rezn5447 <rezn5447@gmail.com>
2016-08-09 11:29:55 -0700

    Start arrays drill

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,36 @@
+# In phase-0-tracks/ruby/arrays_drill.rb, 
+# demonstrate that you can do the following steps, in order. 
+# You'll need to use the Array docs to figure out how to accomplish each one, just as you used the String docs to manipulate strings.
+
+
+
+# Initialize an empty array and store it in a variable (you can choose the name). Print the variable using p.
+drill_array = []
+
+
+# Add five items to your array. Print the array.
+drill_array << ["basketball","baseball","wiffle ball","football","soccer ball"]
+p drill_array
+
+# Delete the item at index 2. Print the array.
+
+
+
+
+# Remove the first item of the array without having to refer to its index. Print the array.
+
+
+
+
+
+# Ask the array whether it includes a certain item. Print the result of this method call (labeled so it prints as more than just an isolated boolean value without any context.)
+
+
+
+
+# Initialize another array that already has a few items in it.
+
+
+
+
+# Add the two arrays together and store them in a new variable. Print the new array.
