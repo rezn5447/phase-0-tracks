962481dfb7244bfa3a91b3fd8a88cce36642b640
rezn5447 <rezn5447@gmail.com>
2016-08-17 14:40:50 -0700

    Add Own Class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -31,7 +31,7 @@ class Puppy
 	  	int.times{"Wag"}
 	end
 
-	end #END PUPPY CLAS
+	end #END PUPPY CLASS
   
  S
 
@@ -46,3 +46,94 @@ ans = gets.chomp
 fido.dog_years(ans)
 
 fido.wag_tail(5)
+
+
+
+
+
+
+#### RELEASE #2 MY OWN CLASS ####
+
+
+
+
+class Hydra
+# Each head is assigned a personality from the array of personalities here:	
+	@@personality_array = ["lazy", "happy","grumpy","dopey","bashful","smarty","murderous", "docile", "bored","menacing","pensive","disinterested"]
+	
+	
+
+	def initialize(heads, name)
+		@name = name
+		@heads = heads
+		@personalities = []
+		@heads.times do @personalities << @@personality_array.sample
+			
+			end
+			
+		p "Emerging from its slumber the vicious #{@name} rears its #{heads} heads!!!"
+	
+	end
+	
+	def heads
+		@heads
+	end
+
+	def personalities()
+		p @personalities
+	end
+	
+	
+	def attacks
+		att_heads = 0
+	@personalities.each do |per|
+	if per == 'murderous' || per == 'grumpy' || per == 'menacing'
+		att_heads += 1
+		p "The #{per} head attacks!"
+	elsif per == 'smarty' || per == 'disinterested'
+	
+		p "This #{per} head chooses not to attack and grabs a book instead."
+	else
+		
+		p "The #{per} head chooses not to attack"
+		end # End of IF statement
+	end # End of EACH statement
+
+	end #End of attacks METHOD
+
+	
+	
+	def cut_off_random
+	
+	rand_id = rand(0...@heads)
+		
+	@heads += 1
+	
+	@personalities.insert(rand_id, @@personality_array.sample, @@personality_array.sample)
+	
+		puts "Despite slicing off head # #{rand_id}, the vicious #{@name} grows two more! They seem #{@personalities[rand_id]} and #{@personalities[rand_id + 1]}."
+	end
+	
+	
+
+end # End Hydra class
+
+### Initialzing 50 Hyrdas!
+### Array to store all values
+hyd_array = []
+i = 1
+while i < 51
+
+## creates new hydras with random number of heads ##
+hyd_array << Hydra.new(rand(2..9),"Tiamat#{i}")
+
+i += 1
+
+end #End creation of hyrda arrays! ** GASP So many hydras! **
+
+### RUNNING CLASS METHODS ON EACH OF THE HYDRAS IN THE ARRAY! SO MANY VALUES!! ###
+hyd_array.each do |x|
+	
+ 	x.heads
+	x.attacks
+end 
