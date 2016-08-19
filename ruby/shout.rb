a8d69c1b69437db494dd5f1ff6be54c266af5973
rezn5447 <rezn5447@gmail.com>
2016-08-18 22:02:03 -0700

    finish release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,5 +2,20 @@ module Shout
 
 	# we'll put some methods here soon, but this code is fine for now!
 
+
+	def self.yell_angrily(words)
+		words.upcase + "!!!" + "    =("
+	end
+
+	def self.yell_happily(words)
+		words.upcase + "!!!" +  "   =D"
+	end
+
+
 end
 
+
+
+p Shout.yell_happily("Wheeeee!")
+
+p Shout.yell_angrily("Crap")
