5b0899465a908f0e07e3088746eefe4e58857faa
rezn5447 <rezn5447@gmail.com>
2016-08-18 22:33:48 -0700

    6-5 Finish

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,21 +1,58 @@
+# module Shout
+
+# 	# we'll put some methods here soon, but this code is fine for now!
+
+
+# 	def self.yell_angrily(words)
+# 		words.upcase + "!!!" + "    =("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words.upcase + "!!!" +  "   =D"
+# 	end
+
+
+# end
+
+
 module Shout
 
-	# we'll put some methods here soon, but this code is fine for now!
 
-
-	def self.yell_angrily(words)
-		words.upcase + "!!!" + "    =("
+	def yell_angrily(words)
+	p	words.upcase + "!!!" + "    =("
 	end
 
-	def self.yell_happily(words)
-		words.upcase + "!!!" +  "   =D"
+	def yell_happily(words)
+	p	words.upcase + "!!!" +  "   =D"
 	end
+end
 
+class Dad
+include Shout	
 
 end
 
 
 
-p Shout.yell_happily("Wheeeee!")
+class Newsie
+include Shout
 
-p Shout.yell_angrily("Crap")
+end
+
+
+loud_dad = Dad.new 
+
+loud_dad.yell_angrily("Clean your room")
+loud_dad.yell_happily("great catch")
+
+
+
+lil_newsie = Newsie.new
+
+lil_newsie.yell_happily("extra extra read all about it")
+lil_newsie.yell_angrily("You have to pay for that paper")
+
+
+
+
+
