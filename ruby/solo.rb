ddfc9a03d714336a9732853222c92d3272725b89
rezn5447 <rezn5447@gmail.com>
2016-08-19 09:33:41 -0700

    6-4 Solo Challenge

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -21,13 +21,12 @@ class Hydra
 # Each head is assigned a personality from the array of personalities here:	
 	@@personality_array = ["lazy", "happy","grumpy","dopey","bashful","smarty","murderous", "docile", "bored","menacing","pensive","disinterested"]
 # Attr Readers and Accessors for later use:	
-	attr_reader :personalities
-	attr_accessor :magic, :heads
+	attr_reader :personalities, :heads
+	attr_accessor :name
 
 	def initialize(heads, name)
 		@name = name
 		@heads = heads
-		@magic = 2 
 		@personalities = []
 		@heads.times do @personalities << @@personality_array.sample
 			
@@ -43,7 +42,7 @@ class Hydra
 ### att_heads check to see if any heads are going to attack and add them to this value
 		att_heads = 0
 
-### Loop through the personalities array of the hydra and perfrom different statements based on the personalities
+### Loop through the personalities array of the hydra and perform different statements based on the personalities ###
 
 	@personalities.each do |per|
 	if per == 'murderous' || per == 'grumpy' || per == 'menacing'
@@ -98,3 +97,64 @@ class Hydra
 	end # END cut_off_head METHOD
 
 end # End Hydra class
+
+
+
+
+
+### START OF USER INPUT PROMPTS ###
+
+hyd_ary = []
+puts "This is Resdan's Hydra generator, You get to fight a monster that you name yourself! What should be the name of this beast?"
+name = gets.chomp
+
+
+puts "You've choosen to fight the #{name}, how many heads does it have? (put 'random' to generate a random number of heads!)"
+heads = gets.chomp!
+ if heads == 'random'
+ 	heads = rand(3..11)
+	else
+		heads = heads.to_i
+		
+end
+
+name = Hydra.new(heads,name)
+hyd_ary << name
+puts "Do you wish to continue conjuring more beasts?? (y / n)"
+
+ans = gets.chomp!
+
+until ans.downcase == 'n'
+### ANSWER CHECK TO MAKE MORE MONSTERS OR END AND LOOP THROUGH ARRAY ###
+if ans.downcase == "y"
+	puts "A bold move!! Lets see if it pays off: What is the name of THIS beast?"
+
+name = gets.chomp
+
+	puts "You've choosen to fight another beast, how many heads does it have? (put 'random' to generate a random number of heads!)"
+		heads = gets.chomp!
+	 if heads == 'random'
+	 	heads = rand(3..11)
+		else
+			heads = heads.to_i
+		end #END heads IF statement
+		name = Hydra.new(heads,name)
+		hyd_ary << name
+elsif ans.downcase == 'n' 
+		puts "Probably a good idea to not make too many 3 ton monsters with multiple murderous heads."
+	p hyd_ary
+### CATCH STATEMENT THAT WILL GENERATE A MONSTER IF ANYTHING BESIDES y OR n IS ENTERED
+else
+	puts "You messed up in your answer and now You have to fight another one anyway!"
+	tiamat = Hydra.new(rand(2..11),'Tiamat')
+	hyd_ary << tiamat
+end 
+puts "Do you wish to continue conjuring more beasts?? (y / n)"
+ans = gets.chomp!
+
+
+end
+puts "Probably a good idea to not make too many 3 ton monsters with multiple murderous heads."
+hyd_ary.each{ |x| p x}
+
+
