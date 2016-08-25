4ee286a1b96e8cc9d220ae862907cbd1cb83ca77
rezn5447 <rezn5447@gmail.com>
2016-08-25 13:51:35 -0700

    7-2 Complete

--- js/data_structures.js
+++ js/data_structures.js
@@ -19,3 +19,21 @@ console.log(horse)
 }
 
 giveHorsiesNames(horseNames,colorsArray)
+
+function Car(color, brand){
+ 
+
+ console.log("A new car just came off the lot")
+
+this.color = color 
+this.brand = brand
+this.numOfWheels = 4
+this.hasHorn = true
+
+console.log("Look at that sweet " + this.color + " " + this.brand + "!")
+
+ this.rev = function(){console.log("** VROOM VROOM **")}
+}
+
+var honda = new Car("red","Honda Civic")
+honda.rev()
