d4d4735f8da6889da28e46ce8580f15b4ae8f77c
rezn5447 <rezn5447@gmail.com>
2016-08-25 13:27:33 -0700

    Release 1 Complete

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,18 @@ var horseNames = ["Sparkles", "Pinky Pie", "Twilight","Zelda"]
 colorsArray.push("ombre")
 
 horseNames.push("Shadowfax")
+
+
+
+function giveHorsiesNames(ary1,ary2){
+
+	var horse = {}
+  for (i = 0; i <= ary1.length - 1; i++ ){
+	horse[ary1[i]] = ary2[i]
+}
+
+console.log(horse)
+
+}
+
+giveHorsiesNames(horseNames,colorsArray)
