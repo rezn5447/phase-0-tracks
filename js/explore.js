5deb3c2b064f1a42552e60fa9726c77cda8b3ab2
rezn5447 <rezn5447@gmail.com>
2016-08-25 12:40:06 -0700

    Pseudocode of reverse function

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// PSEUDOCODE
+// Write a method called 'reverse'
+// Input: STRING
+// Steps:
+// - create VARIABLE to store newReverse
+// - ITERATE from the end of the string
+// - newReverse will append each letter to the VARIABLE as STRING
+// Output:
+// - newReverse VARIABLE as STRING
+
+// ========== END PSEUDOCODE ============ //
+
+
+
+
