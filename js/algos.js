1686a80ced548b3657d114af561be85bcdae6b70
rezn5447 <rezn5447@gmail.com>
2016-08-28 09:26:55 -0700

    Pseudocode for first problem

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+          // ==============   PSEUDOCODE   ================ //
+
+
+// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+// INPUT: ARRAY 
+//  Steps:
+// - ITERATE through to check each element in the array against the other elements, to make sure they are all compared to each other for being the longest
+// Output: Longest element in the array
+
+
+
+        // ==============  END PSEUDOCODE   ================ //
