d5a09493af4b6ee36511fdc9f04345ab980e6d33
rezn5447 <rezn5447@gmail.com>
2016-09-02 14:59:45 -0700

    Add user interface method and includes work fine

--- ruby/8-5/main.rb
+++ ruby/8-5/main.rb
@@ -1,45 +1,26 @@
-# REQUIRE GEMS
 
-require 'sqlite3'
-require 'faker'
+# MOVED GAME TO game.rb #
+require_relative 'game'
+
+# MOVED DATABASE COMMANDS TO database.rb # 
+require_relative 'database'
 
 
 
+database = []
+  def user_interface(database)
+  	puts "here we go!: what Level? (1/2/3)"
+  	input1 = gets.chomp!
+  	database << input1
+  	puts "how many problems? (10/20/30)"
+  	input2 = gets.chomp!
+  	database << input2
 
-db = SQLite3::Database.new("players.db")
-
-  def calculate(operant)
-	correct = 0
-	num1 = rand(10)
-	num2 = rand(10)
-	if operant == '*'
-	  prob = "#{num1} * #{num2}"
-	  ans = num1 * num2
-	elsif operant == '-'
-	  prob = "#{num1} - #{num2}"
-	  ans = num1 - num2
-	else operant == '+'
-	  prob = "#{num1} + #{num2}"
-	  ans = num1 + num2
-	end
-	p prob
-	p ans
+  	difficulty(input1.to_i,input2.to_i)	
   end
-  
 
 
-  def difficulty(level,problems)
-  	i = 0
-  	while i <= problems
-  	oper = (rand(level) + 1)
-  	  if oper >= 3
- 	   calculate('*')
-	  elsif oper == 2
-		calculate('-')
-	  else
-		calculate('+')
-	  end	
-		i += 1
-    end
-  end
-  
+
+
+
+user_interface(database)
